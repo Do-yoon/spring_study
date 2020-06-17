793ed02a0ab0b3d080f5f471fe8a772ab4a4d27d
cheeze <33196437+Do-yoon@users.noreply.github.com>
2020-06-18 04:24:14 +0900

    add test code

---
+++ src/main/java/com/cheeze/study/springboot/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package com.cheeze.study.springboot.service.posts;
+
+import com.cheeze.study.springboot.domain.posts.Posts;
+import com.cheeze.study.springboot.domain.posts.PostsRepository;
+import com.cheeze.study.springboot.web.dto.PostsResponseDto;
+import com.cheeze.study.springboot.web.dto.PostsSaveRequestDto;
+import com.cheeze.study.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
